2f2ba43669b77523ab6d70deb979c18953e8e8c7
romanvasin <roman.v00@inbox.ru>
2025-01-08 08:57:00 +0300

    BI-4: Поправил ссылку на бэк

--- server/api/[...path].ts
+++ server/api/[...path].ts
@@ -1,12 +1,12 @@
 import { proxyRequest, getRequestURL } from 'h3';
 
 /**
- * Проксируем заросы на сервер.
+ * Проксируем запросы через ssr.
  */
 export default defineEventHandler(async (event): Promise<unknown> => {
     const config = useRuntimeConfig();
 
     const reqURL = getRequestURL(event);
 
-    return proxyRequest(event, `${ config.apiBaseURL }${ reqURL.pathname }${ reqURL.search }`);
+    return proxyRequest(event, `${config.apiBaseURL}${reqURL.pathname}${reqURL.search}`);
 });
